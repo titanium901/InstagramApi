9dd2ef1bfc7ccd3c8b05b4d2bda97e0ac0525148
titanium901 <nemes901@hotmail.com>
2019-10-31 16:04:14 +0300

    initial commit

---
+++ InstagramApi/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramApi
+//
+//  Created by Yury Popov on 31.10.2019.
+//  Copyright © 2019 Iurii Popov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstagramApi/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  InstagramApi
+//
+//  Created by Yury Popov on 31.10.2019.
+//  Copyright © 2019 Iurii Popov. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    
+}
+
