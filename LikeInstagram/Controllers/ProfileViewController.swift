b836c1737d86c07d627d7ea42143c6fce36b5b42
titanium901 <nemes901@hotmail.com>
2019-11-02 14:03:17 +0300

    add containerView and refactor animation

--- LikeInstagram/Controllers/ProfileViewController.swift
+++ LikeInstagram/Controllers/ProfileViewController.swift
@@ -11,52 +11,63 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     var isMenuOpen = false
+    var isContainerOpen = false
     
     @IBOutlet weak var slideMenuView: UIView!
-   
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var circleMenuButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        slideMenuView.alpha = 0
     }
     
-    
-    
-    @IBAction func menuButton(_ sender: UIButton) {
-        isMenuOpen.toggle()
-        showSlideMenu()
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.containerView.frame.origin.y = self.view.frame.height
     }
+    
     
     @IBAction func dismissToLoginView(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func showContainerView(_ sender: UIButton) {
+        isContainerOpen.toggle()
+        showContainerView()
+    }
+    @IBAction func menuButton(_ sender: UIButton) {
+         isMenuOpen.toggle()
+         showSlideMenu()
+     }
     
-    
-    func showSlideMenu() {
-        if isMenuOpen {
-            // показываем menu
-            UIView.animate(withDuration: 0.5,
-                           delay: 0,
-                           usingSpringWithDamping: 0.8,
-                           initialSpringVelocity: 0,
-                           options: .curveEaseInOut,
-                           animations: {
-                            self.slideMenuView.frame.origin.x = self.view.frame.width - 140
-            }) { (finished) in
-                
-            }
-        } else {
-            // убираем menu
-            UIView.animate(withDuration: 0.5,
-                           delay: 0,
-                           usingSpringWithDamping: 0.8,
-                           initialSpringVelocity: 0,
-                           options: .curveEaseInOut,
-                           animations: {
-                            self.slideMenuView.frame.origin.x = 0
-            }) { (finished) in
-            }
+    func showContainerView() {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        self.containerView.frame.origin.y = self.isContainerOpen ? self.view.frame.height - self.containerView.frame.height : self.view.frame.height
+                        self.circleMenuButton.transform = CGAffineTransform(rotationAngle: self.isContainerOpen ? CGFloat(Double.pi/4) : CGFloat(Double.pi * 2))
+        }) { (finished) in
+            
         }
     }
+    
+    func showSlideMenu() {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        self.slideMenuView.frame.origin.x = self.isMenuOpen ? self.view.frame.width - 140 : 0
+                        self.menuButton.transform = CGAffineTransform(rotationAngle: self.isMenuOpen ? CGFloat(Double.pi/2) : CGFloat(Double.pi * 2))
+        }) { (finished) in
+            
+        }
+    }
+    
 }
